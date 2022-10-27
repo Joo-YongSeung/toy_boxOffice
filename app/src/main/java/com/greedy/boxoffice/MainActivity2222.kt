7ec6284d97e9065ab1a50c1a403e66071117bfd2
seonghyunki <seong161718@gmail.com>
2022-10-27 15:28:25 +0900

    test

---
+++ app/src/main/java/com/greedy/boxoffice/MainActivity2222.kt
@@ -0,0 +1,11 @@
+package com.greedy.boxoffice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2222 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
