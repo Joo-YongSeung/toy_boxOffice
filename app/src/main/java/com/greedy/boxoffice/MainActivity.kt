a10d141433a7e353e0a4f1e294a675509b80132b
seonghyunki <seong161718@gmail.com>
2022-10-28 17:35:29 +0900

    2022.10.28 용환

--- app/src/main/java/com/greedy/boxoffice/MainActivity.kt
+++ app/src/main/java/com/greedy/boxoffice/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.greedy.boxoffice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.greedy.boxoffice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        setContentView(binding.root)
+
+        binding.testbts.setOnClickListener {
+            val intent = Intent(this, moviedetail::class.java)
+            startActivity(intent)
+
+            MoviesRepository.getPopularMovies()
+        } }
 }
